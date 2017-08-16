9fc97a50f51820c8d305bb7061481d69be929d39
Timothy Britt <tim@timothybritt.com>
2017-08-16 00:34:19 -0500

    update macrocosm store

--- lib/macrocosm.ex
+++ lib/macrocosm.ex
@@ -1,18 +1,40 @@
 defmodule Macrocosm do
-  @moduledoc """
-  Documentation for Macrocosm.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  def create(reducer, initial_state \\ []) do
+    GenServer.start_link(__MODULE__, {reducer, initial_state}, name: __MODULE__)
+  end
 
-  ## Examples
+  def get_state do
+    GenServer.call(__MODULE__, :get_state)
+  end
 
-      iex> Macrocosm.hello
-      :world
+  def get_reducer do
+    GenServer.call(__MODULE__, :get_reducer)
+  end
 
-  """
-  def hello do
-    :world
+  def set_state(new_state) do
+    GenServer.call(__MODULE__, {:set_state, new_state})
+  end
+
+  def animate(action) do
+    GenServer.call(__MODULE__, {:animate, action})
+  end
+
+  def handle_call(:get_state, _from, {reducer, current_state}) do
+    {:reply, current_state, {reducer, current_state}}
+  end
+
+  def handle_call({:set_state, new_state}, _from, {reducer, _current_state}) do
+    {:reply, new_state, {reducer, new_state}}
+  end
+
+  def handle_call(:get_reducer, _from, {reducer, current_state}) do
+    {:reply, reducer, {reducer, current_state}}
+  end
+
+  def handle_call({:animate, action}, _from, {reducer, current_state}) do
+    {new_state, _action} = reducer.(current_state, action)
+    {:reply, {new_state, action}, {reducer, new_state}}
   end
 end
--- test/macrocosm_test.exs
+++ test/macrocosm_test.exs
@@ -1,8 +1,4 @@
 defmodule MacrocosmTest do
   use ExUnit.Case
   doctest Macrocosm
-
-  test "greets the world" do
-    assert Macrocosm.hello() == :world
-  end
 end
